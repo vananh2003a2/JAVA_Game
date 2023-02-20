a8a5082b7de036b4e21fac24a9354375e81cdea8
your name <ntvanh2003a2@gmail.com>
2023-02-20 23:19:14 +0700

    fix background and feedback

--- api/BackgroundAPI.java
+++ api/BackgroundAPI.java
@@ -3,6 +3,7 @@ package com.laptrinhjavaweb.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.laptrinhjavaweb.output.BackgroundOutput;
@@ -14,12 +15,26 @@ public class BackgroundAPI {
 	@Autowired
 	private IBackgroundService backgroundService;
 
-	@GetMapping(value = "akagoe/background")
-	public BackgroundOutput showLink() {
+	@GetMapping(value = "background/{idName}")
+	public BackgroundOutput showLink(@PathVariable("idName") String idName) {
 		BackgroundOutput result = new BackgroundOutput();
-		result.setBackground(backgroundService.findAll());
-		result.setNameOfGame("赤声\r\nChildren's Voices");
-		result.setUrlDownLoad("./assets/file/setup.exe\r\n");
+		result.setBackground(backgroundService.findByIdName(idName));
+		if (idName == "akagoe") {
+			result.setNameOfGame("赤声\r\nChildren's Voices");
+			result.setUrlDownLoad("./assets/file/setup.exe\r\n");
+		}
+		if (idName == "vot") {
+			result.setNameOfGame("vot");
+			result.setUrlDownLoad("./assets/file/setup.exe\r\n");
+		}
+		if (idName == "soulhalo") {
+			result.setNameOfGame("soulhalo");
+			result.setUrlDownLoad("./assets/file/setup.exe\r\n");
+		}
+		if (idName == "revivedknight") {
+			result.setNameOfGame("revivedknight");
+			result.setUrlDownLoad("./assets/file/setup.exe\r\n");
+		}
 		return result;
 	}
 }
